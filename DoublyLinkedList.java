3ae2b64b77e30bb30b80211a999721cc0dff9085
AliaAdnan <97404327+AliaAdnan@users.noreply.github.com>
2022-03-17 19:40:56 +0300

    DSLab8

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -1,99 +1,103 @@
-package com.company;
-
-public class DoublyLinkedList<E> {
+public class DoublyLinkedList <E>  {
     private Node<E>header;
-    private  Node<E>trailer;
-    private int size=0;
-    public  DoublyLinkedList(){
-        header=new Node<E>(null,null,null);
-        //               ELEMENT,PREV,NEXT
-        trailer=new Node<E>(null,header,null);
-        header.setNext(trailer);
+    private Node<E>trailer;
+    private int size =0;
+
+    public DoublyLinkedList() {
+      header=new Node<>(null,null,null);
+      trailer=new Node<>(null,header,null);
+      header.setNext(trailer);
+
     }
     public boolean isEmpty(){
         return size==0;
+
+
     }
     public int size(){
         return size;
     }
-    public  E first(){
+
+    public E frist(){
         if(isEmpty())return null;
         return header.getNext().getElement();
-
-    }
-    public  E last(){
-        if(isEmpty()) return  null;
-        return trailer.getPrev().getElement();
     }
 
-    private void AddBetween(E el,Node<E>p,Node<E>n){
-        Node<E>newest=new Node<E>(el,p,n);
+    public E last(){
+        if(isEmpty())return null;
+        return header.getPrev().getElement();
+    }
+
+    private void addBetween(E el, Node<E>p,Node<E>n){
+        Node<E> newest= new Node<>(el,p,n);
         p.setNext(newest);
         n.setPrev(newest);
         size++;
+
     }
-    public void  AddFrist(E el){
-        AddBetween(el,header,header.getNext());
+
+public void addFirst(E el){
+
+        addBetween(el,header,header.getNext());
+}
+    public void addLast(E el){
+        addBetween(el,trailer.getPrev(),trailer);
     }
-    public void  AddLast(E el){
-        AddBetween(el,trailer.getPrev(),trailer);
-    }
-    private  E remove(Node<E>x){
-        Node<E> p=x.getPrev();
-        Node<E>n=x.getNext();
+
+private E remove(Node<E>x){
+        Node<E>p=x.getPrev();
+        Node<E>n=x.getPrev();
         p.setNext(n);
         n.setPrev(p);
         size--;
         return x.getElement();
 
-    }
-    public E removeFirst(){
-        if(isEmpty())return null;
+}
+public E removeFrist(){
         return remove(header.getNext());
 
-    }
-    public  E removeLastr()
-    {
-        if(isEmpty())return null;
+}
+public E removeLast(){
         return remove(trailer.getPrev());
+}
+
+
+
+
+    private static class  Node<E>{
+        E  element;
+        Node<E> prev;
+        Node<E>next;
+
+        public Node(E element, Node<E> prev, Node<E> next) {
+            this.element = element;
+            this.prev = prev;
+            this.next = next;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public void setElement(E element) {
+            this.element = element;
+        }
+
+        public Node<E> getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node<E> prev) {
+            this.prev = prev;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
     }
 
-        private static  class Node<E>
-        {
-            E element ;
-            Node<E> prev;
-            Node<E> next;
-
-            public Node(E element, Node<E> prev, Node<E> next) {
-                this.element = element;
-                this.prev = prev;
-                this.next = next;
-            }
-
-            public E getElement() {
-                return element;
-            }
-
-            public void setElement(E element) {
-                this.element = element;
-            }
-
-            public Node<E> getPrev() {
-                return prev;
-            }
-
-            public void setPrev(Node<E> prev) {
-                this.prev = prev;
-            }
-
-            public Node<E> getNext() {
-                return next;
-            }
-
-            public void setNext(Node<E> next) {
-                this.next = next;
-            }}}
-
-
-
-
+}
--- TestDoubly.java
+++ TestDoubly.java
@@ -1,38 +1,38 @@
-package com.company;
-import java.util.List;
 import java.util.Scanner;
 
 public class TestDoubly {
     public static void main(String[] args) {
-
-        DoublyLinkedList<String>list=new DoublyLinkedList<>();
-        Scanner in =new Scanner(System.in);
-        System.out.println("1 add first\t 2 add last \t 3 remove first\t4remove last \t 0 exit");
+        DoublyLinkedList<String>list =new DoublyLinkedList<>();
+        Scanner in= new Scanner(System.in);
         int choice=-1;
-        while (choice!=0)
-        {
-            System.out.println("your choice");
-            choice=in.nextInt();
-            switch (choice){
-                case 1:
-                    System.out.println("input a name");
-                    list.AddFrist(in.next());
-                    break;
-                case 2:
-                    System.out.println("input a name");
-                    list.AddLast(in.next());
-                    break;
-                case 3:
-                    System.out.println("remove is "+list.removeFirst());
-                    break;
-                case 4:
-                    System.out.println("remove is "+list.removeLastr());
-                    break;
-                case 0:
-                    System.exit(0);
-                    break;
-            }
-            System.out.println("first  "+list.first()+"\tlast = "+list.last()+"\t size = "+list.size());
-        }
-    }}
+        System.out.println(" 1 add frist \t 2 add last\t 3 remove frist \t 4 remove last \t 0 exit");
+        while (choice!=0){
 
+            System.out.println("your choice :");
+          choice=in.nextInt();
+          switch (choice){
+              case 0:
+                  System.exit(0);
+                  break;
+              case 1:
+                  System.out.println("input a name ");
+                  list.addFirst(in.next());
+                  break;
+              case 2:
+                  System.out.println("input a name ");
+                  list.addLast(in.next());
+                  break;
+              case 3:
+                  System.out.println(" removed is :"+list.removeFrist());
+                  break;
+              case 4:
+                  System.out.println("removed is :"+ list.removeLast());
+                  break;
+//              System.out.println("frist ="+list.frist()+"last");
+
+
+            }
+
+        }
+    }
+}
